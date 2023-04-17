85b04df2bbcb7d9a02a7776d55b2b78adaf6857e
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-04-17 14:51:17 -0600

    update ci-cd #4

--- teams/urls.py
+++ teams/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 
 from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
+from django.conf.urls import include, url
 
 urlpatterns = [
+    url(r'^ht/', include('health_check.urls')),
     path('admin/', admin.site.urls),
     path('graphql/', csrf_exempt(GraphQLView.as_view(graphiql=True))),
 ]
