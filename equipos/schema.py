c6514cce1086e5184ac1f61b7d86ca0c90aa9cff
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-05-29 14:10:43 -0600

    subir filtering

--- equipos/schema.py
+++ equipos/schema.py
@@ -2,6 +2,19 @@ import graphene
 from graphene_django import DjangoObjectType
 
 from .models import Equipo
+from users.schema import UserType
+
+from equipos.models import Equipo, Vote
+from graphql import GraphQLError
+from django.db.models import Q
+
+
+
+# ...code
+# Add after the LinkType
+class VoteType(DjangoObjectType):
+    class Meta:
+        model = Vote
 
 
 class EquipoType(DjangoObjectType):
@@ -10,10 +23,23 @@ class EquipoType(DjangoObjectType):
 
 
 class Query(graphene.ObjectType):
-    equipos = graphene.List(EquipoType)
+    equipos = graphene.List(EquipoType, search=graphene.String())
+    votes = graphene.List(VoteType)
 
-    def resolve_equipos(self, info, **kwargs):
+
+    def resolve_equipos(self, info, search=None, **kwargs):
+        # The value sent with the search parameter will be in the args variable
+        if search:
+            filter = (
+                Q(nombre__icontains=search) |
+                Q(continente__icontains=search)
+            )
+            return Equipo.objects.filter(filter)
+
         return Equipo.objects.all()
+
+    def resolve_votes(self, info, **kwargs):
+        return Vote.objects.all()
 
 class CreateEquipo(graphene.Mutation):
     id = graphene.Int()
@@ -27,6 +53,7 @@ class CreateEquipo(graphene.Mutation):
     trofeosinte = graphene.Int()
     trofeosloca = graphene.Int()
     numerojuga = graphene.Int()
+    posted_by = graphene.Field(UserType)
 
 
     #2
@@ -44,7 +71,8 @@ class CreateEquipo(graphene.Mutation):
 
     #3
     def mutate(self, info, nombre, fundacion, continente, trofeos, presidente, pais, liga, trofeosinte, trofeosloca, numerojuga):
-        equipo = Equipo(nombre=nombre, fundacion=fundacion, continente=continente, trofeos=trofeos, presidente=presidente, pais=pais, liga=liga, trofeosinte=trofeosinte, trofeosloca=trofeosloca, numerojuga=numerojuga)
+        user = info.context.user or None
+        equipo = Equipo(nombre=nombre, fundacion=fundacion, continente=continente, trofeos=trofeos, presidente=presidente, pais=pais, liga=liga, trofeosinte=trofeosinte, trofeosloca=trofeosloca, numerojuga=numerojuga, posted_by=user,)
         equipo.save()
 
         return CreateEquipo(
@@ -59,11 +87,43 @@ class CreateEquipo(graphene.Mutation):
             trofeosinte=equipo.trofeosinte,
             trofeosloca=equipo.trofeosloca,
             numerojuga=equipo.numerojuga,
+            posted_by=equipo.posted_by,
         )
 
+class CreateVote(graphene.Mutation):
+    user = graphene.Field(UserType)
+    equipo = graphene.Field(EquipoType)
 
-#4
+    class Arguments:
+        equipo_id = graphene.Int()
+
+    def mutate(self, info, equipo_id):
+        user = info.context.user
+        if user.is_anonymous:
+            raise GraphQLError('You must be logged to vote!')
+
+
+        equipo = Equipo.objects.filter(id=equipo_id).first()
+        if not equipo:
+            raise Exception('Invalid Link!')
+
+        Vote.objects.create(
+            user=user,
+            equipo=equipo,
+        )
+
+        return CreateVote(user=user, equipo=equipo)
+    
+
+# ...code
+# Add the mutation to the Mutation class
 class Mutation(graphene.ObjectType):
     create_equipo = CreateEquipo.Field()
+    create_vote = CreateVote.Field()
+
+
 
 schema = graphene.Schema(query=Query, mutation=Mutation)
+
+
+
